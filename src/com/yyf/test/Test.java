cd3973aaf2d3f24b2ab6ebba1578c72e44417dde
yunfei <291234457@qq.com>
2018-10-16 09:36:12 +0800

    java调用webservice接口，需连接外网才可以

---
+++ src/com/yyf/test/Test.java
@@ -0,0 +1,16 @@
+package com.yyf.test;
+
+import com.yyf.weather.ArrayOfString;
+import com.yyf.weather.WeatherWebService;
+import com.yyf.weather.WeatherWebServiceSoap;
+
+public class Test {
+	public static void main(String[] args) {
+		WeatherWebService wws = new WeatherWebService();
+		WeatherWebServiceSoap wwss = wws.getWeatherWebServiceSoap();
+		ArrayOfString weatherbyCityName = wwss.getWeatherbyCityName("…œ∫£");
+		for (String st : weatherbyCityName.getString()) {
+						System.out.println(st);
+		}
+	}
+}
